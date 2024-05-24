f97c0944b85807a2d5c7854affb8728d665ef07b
JalenZS <jalenzscrivner@gmail.com>
2024-05-23 21:21:39 -0700

    switched to hash router, possible gh pages fix

--- chordhelp/src/App.js
+++ chordhelp/src/App.js
@@ -1,15 +1,15 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import ErrorPage from './pages/ErrorPage';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="*" element={<ErrorPage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
